25faf2f62b262ef7297ff6a143783b39f0ed6f16
hyx0329 <hyx0329@163.com>
2022-02-26 14:20:47 +0800

    添加多用户支持
    Signed-off-by: hyx0329 <hyx0329@163.com>

--- Gboard_dict_3/customize.sh
+++ Gboard_dict_3/customize.sh
@@ -1,19 +1,40 @@
 # 注意 这不是占位符！！这个代码的作用是将模块里的东西全部塞系统里，然后挂上默认权限
 SKIPUNZIP=0
 
-uid=$(ls -n /data/data | grep "com.google.android.inputmethod.latin" | awk '{print $3}')
-if [ -z "$uid" ]; then
-    abort "请检查是否安装 Gboard"
-    abort "退出安装"
-fi
+install_counter=0
 
 if [ -z "$MODPATH" ]; then
-    MODPATH="."
+  MODPATH="."
 fi
 
-mv /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3 /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3.bak
-cp -f $MODPATH/user_dict_3_3 /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-chown $uid /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-chgrp $uid /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-chmod 600 /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-am force-stop com.google.android.inputmethod.latin
+users=$(ls /data/user)
+
+for user in $users; do
+
+  uid=$(ls -n /data/user/$user | grep "com.google.android.inputmethod.latin" | awk '{print $3}')
+  if [ -z "$uid" ]; then
+    echo "- 用户 $user 没有安装Gboard"
+    echo "- 跳过"
+    continue
+  fi
+
+  echo "- 为用户 $user 安装词库"
+  DICT_FILE=/data/user/$user/com.google.android.inputmethod.latin/files/user_dict_3_3
+  mv $DICT_FILE $DICT_FILE.bak
+  cp -f $MODPATH/user_dict_3_3 $DICT_FILE
+  chown $uid $DICT_FILE
+  chgrp $uid $DICT_FILE
+  chmod 600 $DICT_FILE
+
+  am force-stop --user $user com.google.android.inputmethod.latin
+
+  install_counter=$(echo $install_counter + 1 | bc)
+
+done
+
+echo "- 有" $install_counter "位用户安装了词库"
+
+if [ 0 -eq $install_counter ]; then
+  abort "- 没有用户安装有Gboard"
+  abort "- 终止安装"
+fi
--- Gboard_dict_3/uninstall.sh
+++ Gboard_dict_3/uninstall.sh
@@ -1,10 +1,14 @@
 #!/system/bin/sh
 
-uid=$(ls -n /data/data | grep "com.google.android.inputmethod.latin" | awk '{print $3}')
-if [ -n "$uid" ]; then
-    mv /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3.bak /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-    chown $uid /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-    chgrp $uid /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-    chmod 600 /data/data/com.google.android.inputmethod.latin/files/user_dict_3_3
-    am force-stop com.google.android.inputmethod.latin
-fi
+for user in $(ls /data/user); do
+  uid=$(ls -n /data/user/$user | grep "com.google.android.inputmethod.latin" | awk '{print $3}')
+  if [ -n "$uid" ]; then
+    echo "- 尝试为用户 $user 恢复备份(如果有)"
+    DICT_FILE=/data/user/$user/com.google.android.inputmethod.latin/files/user_dict_3_3
+    mv $DICT_FILE.bak $DICT_FILE
+    chown $uid $DICT_FILE
+    chgrp $uid $DICT_FILE
+    chmod 600 $DICT_FILE
+    am force-stop --user $user com.google.android.inputmethod.latin
+  fi
+done
